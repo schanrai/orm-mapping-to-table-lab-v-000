49b781506287e837b93263608997dbc624a14bf3
Sushi Chanrai <sushi@pathworks.co>
2018-08-22 20:03:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,6 +8,16 @@ def initialize(name, grade)
   @grade = grade
 end
 
+  def self.create_table
+    sql = <<-SQL 
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
