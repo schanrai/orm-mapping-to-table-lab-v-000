3adca21147ca4e209dabf31415c99ef1a66b726f
Sushi Chanrai <sushi@pathworks.co>
2018-08-22 20:07:19 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,7 +21,7 @@ end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
-def drop_table
+def self.drop_table
   sql = <<-SQL
     DROP TABLE students
   SQL
