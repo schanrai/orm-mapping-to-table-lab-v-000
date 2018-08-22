5bb1dd1fc5738df9b739f3d47ca3fcd8a8fb8eac
Sushi Chanrai <sushi@pathworks.co>
2018-08-22 20:14:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,8 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(name, grade)
+  def initialize(name, grade, id=nil)
+    @id = id
     @name = name
     @grade = grade
   end
