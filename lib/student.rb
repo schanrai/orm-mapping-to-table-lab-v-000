ad31ee348525d9819e930bfcad435c2a5ba18771
Sushi Chanrai <sushi@pathworks.co>
2018-08-22 20:12:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,47 +3,41 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-def initialize(name, grade)
-  @name = name
-  @grade = grade
-end
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
+  end
 
-def self.create_table
-  sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        grade TEXT
-        )
-        SQL
-  DB[:conn].execute(sql)
-end
+  def self.create_table
+    sql = <<-SQL
+        CREATE TABLE IF NOT EXISTS students (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          grade TEXT
+          )
+          SQL
+    DB[:conn].execute(sql)
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
-def self.drop_table
-  sql = <<-SQL
-    DROP TABLE students
-  SQL
-  DB[:conn].execute(sql)
-end
-
-def save
-  sql = <<-SQL
-    INSERT INTO students (name, grade)
-    VALUES (?, ?)
-  SQL
-end
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+    SQL
+    DB[:conn].execute(sql)
+  end
 
   def save
     sql = <<-SQL
-      INSERT INTO songs (name, album)
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
-
-    DB[:conn].execute(sql, self.name, self.album)
-
+    DB[:conn].execute(sql, self.name, self.grade)
   end
+
+
+
 
 
 
